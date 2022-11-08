7d15ca193937b1c39e235cc598bcd541d5df43e2
Maria <mtens17@gmail.com>
2022-11-08 14:48:21 +0000

    Add stored emojis functionality

--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -5,19 +5,28 @@ export default function EmojiPicker(): JSX.Element {
     useState('');
     const [previousEmojiFromCurrentRender, queueRerenderWithNewpreviousEmoji] =
     useState('');
+    const [storedEmojisFromCurrentRender, queueRerenderWithNewEmojis] =
+    useState<string[]>([]);
+
+    const handleStoreEmojis = () => {
+      queueRerenderWithNewEmojis([
+        ...storedEmojisFromCurrentRender,
+        emojiFromCurrentRender,
+      ]);
+    };
 
     return (
         <>
           <h1>Emoji picker</h1>
-          {/* <h2>Previous emoji: {previousEmojiFromCurrentRender} </h2>  */}
           <h2> Current emoji: {emojiFromCurrentRender}</h2>
           <h2> Previous emoji: {previousEmojiFromCurrentRender}</h2>
+          <h2>Your stored emojis: {storedEmojisFromCurrentRender.join(", ")}</h2>
           <hr />
-          <button onClick={() => {queueRerenderWithNewEmoji('🍳'); queueRerenderWithNewpreviousEmoji(emojiFromCurrentRender)}}>🍳</button>
-          <button onClick={() => {queueRerenderWithNewEmoji('🍽️'); queueRerenderWithNewpreviousEmoji(emojiFromCurrentRender)}}>🍽️</button>
-          <button onClick={() => {queueRerenderWithNewEmoji('⏰'); queueRerenderWithNewpreviousEmoji(emojiFromCurrentRender)}}>⏰</button>
-          <button onClick={() => {queueRerenderWithNewEmoji('😋'); queueRerenderWithNewpreviousEmoji(emojiFromCurrentRender)}}>😋</button>
-          <button onClick={() => {queueRerenderWithNewEmoji('🛌'); queueRerenderWithNewpreviousEmoji(emojiFromCurrentRender)}}>🛌</button>
+          <button onClick={() => {queueRerenderWithNewEmoji('🍳'); queueRerenderWithNewpreviousEmoji(emojiFromCurrentRender); handleStoreEmojis()}}>🍳</button>
+          <button onClick={() => {queueRerenderWithNewEmoji('🍽️'); queueRerenderWithNewpreviousEmoji(emojiFromCurrentRender); handleStoreEmojis()}}>🍽️</button>
+          <button onClick={() => {queueRerenderWithNewEmoji('⏰'); queueRerenderWithNewpreviousEmoji(emojiFromCurrentRender); handleStoreEmojis()}}>⏰</button>
+          <button onClick={() => {queueRerenderWithNewEmoji('😋'); queueRerenderWithNewpreviousEmoji(emojiFromCurrentRender); handleStoreEmojis()}}>😋</button>
+          <button onClick={() => {queueRerenderWithNewEmoji('🛌'); queueRerenderWithNewpreviousEmoji(emojiFromCurrentRender); handleStoreEmojis()}}>🛌</button>
         </>
     );
 }
