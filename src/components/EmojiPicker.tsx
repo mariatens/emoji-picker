1b5ca61950296739901c5a68c432973f68261207
Maria <mtens17@gmail.com>
2022-11-09 12:53:43 +0000

    Add map functionality and tidy onclick code into separate functions

--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -1,32 +1,63 @@
 import { useState } from "react";
 
+interface Emoji {
+  key: string
+}
+const emojis: Emoji[] = []
+
+function EmojiStore(props: Emoji){
+  return(
+    <>
+     <li>{props.key}</li>
+    </>
+  )
+}
+// in every on click I want to update an array
+
 export default function EmojiPicker(): JSX.Element {
     const [emojiFromCurrentRender, queueRerenderWithNewEmoji] =
     useState('');
     const [previousEmojiFromCurrentRender, queueRerenderWithNewPreviousEmoji] =
     useState('');
-    const [storedEmojisFromCurrentRender, queueRerenderWithNewEmojis] =
-    useState<string[]>([]);
 
-    const handleStoreEmojis = () => {
-      queueRerenderWithNewEmojis([
-        ...storedEmojisFromCurrentRender,
-        emojiFromCurrentRender,
-      ]);
-    };
+    const handleEggEmoji = () => {
+      queueRerenderWithNewEmoji('🍳'); 
+      emojis.push({key: '🍳'}); 
+      queueRerenderWithNewPreviousEmoji(emojiFromCurrentRender)
+    }
+    const handlePlateEmoji = () => {
+      queueRerenderWithNewEmoji('🍽️'); 
+      emojis.push({key: '🍽️'});
+      queueRerenderWithNewPreviousEmoji(emojiFromCurrentRender);
+    }
+    const handleClockEmoji = () =>{
+       queueRerenderWithNewEmoji('⏰');
+       emojis.push({key: '⏰'});
+       queueRerenderWithNewPreviousEmoji(emojiFromCurrentRender);
+    }
+    const handleTongueEmoji = () =>{
+      queueRerenderWithNewEmoji('😋'); 
+      emojis.push({key: '😋'}); 
+      queueRerenderWithNewPreviousEmoji(emojiFromCurrentRender)
+    }
+    const handleBedEmoji = () =>{
+      queueRerenderWithNewEmoji('🛌'); 
+      emojis.push({key: '🛌'});
+       queueRerenderWithNewPreviousEmoji(emojiFromCurrentRender)
+    }
 
     return (
         <>
           <h1>Emoji picker</h1>
           <h2> Current emoji: {emojiFromCurrentRender}</h2>
           <h2> Previous emoji: {previousEmojiFromCurrentRender}</h2>
-          <h2> Your stored emojis: {storedEmojisFromCurrentRender.join(',')}</h2>
-          <hr />
-          <button onClick={() => {queueRerenderWithNewEmoji('🍳'); queueRerenderWithNewPreviousEmoji(emojiFromCurrentRender); handleStoreEmojis()}}>🍳</button>
-          <button onClick={() => {queueRerenderWithNewEmoji('🍽️'); queueRerenderWithNewPreviousEmoji(emojiFromCurrentRender); handleStoreEmojis()}}>🍽️</button>
-          <button onClick={() => {queueRerenderWithNewEmoji('⏰'); queueRerenderWithNewPreviousEmoji(emojiFromCurrentRender); handleStoreEmojis()}}>⏰</button>
-          <button onClick={() => {queueRerenderWithNewEmoji('😋'); queueRerenderWithNewPreviousEmoji(emojiFromCurrentRender); handleStoreEmojis()}}>😋</button>
-          <button onClick={() => {queueRerenderWithNewEmoji('🛌'); queueRerenderWithNewPreviousEmoji(emojiFromCurrentRender); handleStoreEmojis()}}>🛌</button>
+          <h2> Your stored emojis: {emojis.map(EmojiStore)}</h2> 
+          <hr /> 
+          <button onClick={handleEggEmoji}>🍳</button>
+          <button onClick={handlePlateEmoji}>🍽️</button>
+          <button onClick={handleClockEmoji}>⏰</button>
+          <button onClick={handleTongueEmoji}>😋</button>
+          <button onClick={handleBedEmoji}>🛌</button>
         </>
     );
 }
